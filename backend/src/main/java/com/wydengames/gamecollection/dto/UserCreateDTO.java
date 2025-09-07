ad20254389a5f9e36812cca73a66efdfe6228cd1
danieloliveeira <danieldoskil@gmail.com>
2025-09-07 17:58:51 -0300

    feat: Adiciona novo dto de request

--- backend/src/main/java/com/wydengames/gamecollection/dto/UserCreateDTO.java
+++ backend/src/main/java/com/wydengames/gamecollection/dto/UserCreateDTO.java
@@ -1,4 +1,14 @@
 package com.wydengames.gamecollection.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
 public class UserCreateDTO {
+    private String username;
+    private String email;
+    private String password;
 }
