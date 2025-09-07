9987f83d7fbe7dacc666c9d430150cd69abaeb88
danieloliveeira <danieldoskil@gmail.com>
2025-09-07 18:00:16 -0300

    feat: Adiciona novo repository

--- backend/src/main/java/com/wydengames/gamecollection/repositories/UserRepository.java
+++ backend/src/main/java/com/wydengames/gamecollection/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.wydengames.gamecollection.repositories;
 
-public interface UserRepository {
+import com.wydengames.gamecollection.entites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findByEmail(String email);
 }
