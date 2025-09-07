9545a794a64b1fc5ec70c2c42c13454ec38ec9c6
danieloliveeira <danieldoskil@gmail.com>
2025-09-07 17:59:57 -0300

    feat: Adiciona nova entidade

--- backend/src/main/java/com/wydengames/gamecollection/entites/User.java
+++ backend/src/main/java/com/wydengames/gamecollection/entites/User.java
@@ -1,4 +1,50 @@
 package com.wydengames.gamecollection.entites;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column( nullable = false, unique = true)
+    private String username;
+
+    @Column( nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
