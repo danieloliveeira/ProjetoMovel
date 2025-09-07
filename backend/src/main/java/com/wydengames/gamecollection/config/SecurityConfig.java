9af8626870910e34661eebea7e5fa887ffecc109
danieloliveeira <danieldoskil@gmail.com>
2025-09-07 17:57:00 -0300

    feat: Adiciona arquivo de seguranca

--- backend/src/main/java/com/wydengames/gamecollection/config/SecurityConfig.java
+++ backend/src/main/java/com/wydengames/gamecollection/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy; // Importe esta classe
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -46,9 +46,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())) // Movi para cima por organização
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
 
-                // 👇 ADIÇÃO IMPORTANTE 👇
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
@@ -56,7 +55,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
 
-                // 👇 ADIÇÃO IMPORTANTE 👇
                 .httpBasic(Customizer.withDefaults()); // Habilita explicitamente o HTTP Basic Auth
 
         return http.build();
